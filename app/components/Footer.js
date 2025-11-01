853fb2b85adbc2b65236d452d2a596b33f999d10
Charan <charancharan2243@gmail.com>
2025-11-01 14:57:09 +0530

    small correction

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
    
     <div className="w-full md:h-[250px] bg-stone-700 flex md:flex-row flex-col gap-x-200 pb-5">
         <img src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Flogo-light.png&w=1920&q=75" alt="Logo" className="w-[150px] rounded- h-[50px] ml-32 md:ml-25 mt-20 md:mt-35 "/>
->>>>>>> 125aa5e (home res)
+
 
         <p className="md:mt-40 font-semibold text-sm md:ml-0 ml-12 mt-2 md:text-md text-shadow-gray-800">© Copyright 2025. Restan. All Rights Reserved</p>
     </div>
