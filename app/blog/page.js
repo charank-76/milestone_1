d9993d1f2ce61fa4a1546b192d40a34f724724d7
Charan <charancharan2243@gmail.com>
2025-11-01 10:56:32 +0530

    blog done

---
+++ app/blog/page.js
@@ -0,0 +1,57 @@
+"use client"
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Aboutus from "../components/Aboutus";
+import BlogList from "../components/BlogList";
+
+export default function Blog() {
+    const blogs = [
+        {
+            imgurl: "https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fblog%2F1.jpg&w=1920&q=75",
+            title: "Picked up a Brussels burger Sprouts.",
+            date: "12 August, 2024",
+            author: " john Baus"
+        },
+        {
+            imgurl: "https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fblog%2F2-full.jpg&w=3840&q=75",
+            title: "This prefabricated passive house highly",
+            date: "13 August, 2024",
+            author: "Admin"
+        },
+        {
+            imgurl: "https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fblog%2F3.jpg&w=1920&q=75",
+            title: "The best food recipes for you",
+            date: "14 August, 2024",
+            author: " john Baus"
+        }]
+  return (
+    <div className="bg-white w-full min-h-screen flex flex-col items-center  ">
+        <Navbar />
+         <div className="flex flex-col  mt-20 ">
+                {blogs.map((element) =>{
+                    return(
+                        <BlogList
+                      key={element.title}
+                        title={element.title}
+                       
+                        date={element.date}
+                        author={element.author}
+                        imgurl={element.imgurl}
+                        
+                        />      
+                    )
+                }
+            )}</div>
+            <div className="flex gap-2 mt-10 mb-10   ">
+                <button className="bg-green-900 text-white px-4 py-2 rounded-md hover:bg-black ">1</button>
+                <button className="bg-gray-300 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white ">2</button>
+                <button className="bg-gray-300 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white ">3</button>
+                 <button className="bg-gray-300 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white ">➜</button>
+
+            </div>
+<div className="px-20 border-b-2 border-gray-500"><Aboutus /></div>
+
+        <Footer  />
+        </div>
+        )}
---
+++ app/components/BlogList.js
@@ -0,0 +1,20 @@
+"use client"
+
+export default function BlogList({imgurl,title,date,author}) {
+    return (
+        <div className="flex flex-col border border-gray-300 bg-white  shadow-md rounded-3xl mt-10 mb-10 w-[1000px] ">
+            <img
+                src={imgurl}
+                className="w-full h-[500px] object-cover rounded-xl"/>
+                 <div className="flex gap-8 text-gray-400 text-lg mt-10 px-10 ">
+                    <p>{date}</p>
+                    <p>👤 {author}</p>
+                </div>
+                <div className="flex flex-col text-2xl w-full h-[150px] text-md font-light text-gray-500  mt-5 px-10">
+                    <p className="text-4xl font-bold text-black  ">{title}</p>
+                    <p className=" text-lg font-sans mt-5 ">Bndulgence diminution so discovered mr apartments. Are off under folly death wrote cause her way spite. Plan upon yet way get cold spot its week. Almost do am or limits hearts. Resolve parties but why she shewing. She sang know now</p>
+                </div>
+                <button className="bg-green-900 text-white font-bold w-[150px] h-[60px] mt-5 rounded-full mb-10 ml-10 hover:bg-black ">Read More</button>
+              </div>
+    )
+}
